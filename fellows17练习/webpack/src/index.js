7f85fab32ae675f1b1a290797c781541927a5bb9
Caiweiqi <iscaiweiqi@163.com>
2018-01-27 16:14:32 +0800

    js

--- fellows17练习/webpack/src/index.js
+++ fellows17练习/webpack/src/index.js
@@ -1,2 +1,8 @@
 import css from './css/index.css';
+import sass from './css/common.scss';
+import $ from 'jquery';
+let json = require('../author');
+let a = 1;
 document.write('xixi111122211');
+console.log(json);
+$('#div1').css('color','blue').html(json.name);
--- fellows17练习/webpack/webpack.config.js
+++ fellows17练习/webpack/webpack.config.js
@@ -2,23 +2,80 @@ const path = require('path');
 const devServer = require('webpack-dev-server');
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const webpack = require('webpack');
+var  CopyWebpackPlugin = require('copy-webpack-plugin');
+/* const entry = require('./webpack_config/entry_webpack') */
 module.exports = {
-    entry:{
-        index:'./src/index.js'
-    },
+    entry:
+        {index:'./src/index.js',
+          jquery:'jquery',
+          vue:'vue'
+        },
+    
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+        filename:'[name].js',
+        publicPath:'http://localhost:8081/'
     },
     module:{
         rules:[
             {
                 test:/\.css$/,
-              //  use:['style-loader','css-loader']
+               // use:['style-loader','css-loader']
                 use:ExtractTextPlugin.extract({
                     fallback:'style-loader',
-                    use:'css-loader'
+                    use:[{
+                        loader:'css-loader',
+                        options:{imporLoaders:1}
+                    },'postcss-loader']
                 })
+            },{
+                test:/\.(png|jpg|gif)/,
+                use:[{
+                    loader:'url-loader',
+                    options:{
+                        limit:500,
+                        outputPath:'images/'
+                    }
+                }]
+            },{
+                test:/\.(htm|html)$/i,
+                loader:'html-withimg-loader'
+            },{
+                test:/\.scss/,
+              //  use:[
+               //     {
+                 //       loader:'style-loader'
+               //     },
+                //    {
+                //        loader:'css-loader'
+                //    },
+                //    {
+                //        loader:'sass-loader'
+                 //   }
+               // ]
+               use:ExtractTextPlugin.extract({
+                     use:[{
+                         loader:'css-loader'
+                     },{
+                         loader:'sass-loader'
+                     }],
+                     fallback:'style-loader'
+                 })
+            },{
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:[
+                            'es2015','react','env'
+                        ]
+                    }
+                },
+                exclude:/node_modules/
             }
         ]
         
@@ -31,7 +88,27 @@ module.exports = {
             hash:true,
             template:'./src/index.html'
         }),
-        new ExtractTextPlugin('css/index.css')
+        new ExtractTextPlugin('css/index.css'),
+        //new UglifyJsPlugin(),
+        new PurifyCSSPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*.html')),
+        }),
+        new webpack.BannerPlugin('今晚打老虎'),
+       // new webpack.ProvidePlugin({
+         //   $:'jquery'
+       // }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:['jquery','vue','index'],
+            filename:'js/[name].js',
+           // name:'jquery',
+            //filename:'js/jquery.js',
+            minChunks:2
+        }),
+        new CopyWebpackPlugin([{
+            from:__dirname + '/src',
+            to:'./public'
+        }])
+        
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
