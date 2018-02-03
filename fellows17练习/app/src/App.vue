d0f3c82b380add6dea542b099ca63d898d4c3faf
Caiweiqi <iscaiweiqi@163.com>
2018-02-03 23:00:55 +0800

    APP

---
+++ fellows17练习/app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+//import rem from '@/assets/js/rem'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ fellows17练习/app/src/components/movies/MovieList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <top></top>
+        movie
+        <bottom></bottom>
+    </div>
+</template>
+
+<script>
+    import top from '@/components/common/Top'
+    import bottom from '@/components/common/Bottom'
+    export default {
+        components:{
+            top,
+            bottom
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
