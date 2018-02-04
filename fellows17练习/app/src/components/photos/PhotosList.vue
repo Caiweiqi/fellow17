db500def541e320e68b3a39a2d65fdac2b187094
Caiweiqi <iscaiweiqi@163.com>
2018-02-04 22:47:33 +0800

    photo

---
+++ fellows17练习/app/src/components/photos/PhotosList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+         mounted:function(){
+            this.$store.commit('change','rgb(64,224,208)'),
+            this.$store.commit('changes','Photo')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
