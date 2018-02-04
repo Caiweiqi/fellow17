c190aa6cc5b0ce59a15ef519a6626633fdd8bd1d
Caiweiqi <iscaiweiqi@163.com>
2018-02-04 22:48:02 +0800

    music

---
+++ fellows17练习/app/src/components/musics/MusicsList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       music
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted:function(){
+            this.$store.commit('change','rgb(0,150,136)'),
+            this.$store.commit('changes','Music')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
