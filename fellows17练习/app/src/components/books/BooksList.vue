9e2640618c900c4eb9b0c7c3581768a831700271
Caiweiqi <iscaiweiqi@163.com>
2018-02-05 15:38:31 +0800

    book

--- fellows17练习/app/src/components/books/BooksList.vue
+++ fellows17练习/app/src/components/books/BooksList.vue
@@ -5,27 +5,39 @@
             <swipe-item class="slide2"></swipe-item>
             <swipe-item class="slide3"></swipe-item>
         </swipe>
+        <li></li>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import vueswipe  from 'vue-swipe/dist/vue-swipe.css'
     import { Swipe, SwipeItem } from 'vue-swipe';
     export default {
-          mounted:function(){
-            this.$store.commit('change','rgb(255,105,180)'),
-            this.$store.commit('changes','Book')
-        },
         components:{
             Swipe,
             SwipeItem
-        }
+        },
+        data() {
+            return {
+                bookList:[]
+            }
+        },
+        mounted:function(){
+            this.$store.commit('change','rgb(255,105,180)'),
+            this.$store.commit('changes','Book')
+
+            // axios.get(API_INTERFACE+'http://m.maoyan.com/movie/list.json?type=hot&offset=&limit=10').then((res)=>{
+            //    this.bookList = res.
+            // })
+        },
+        
     }
 </script>
 
 <style scoped>
     .my-swipe {
-    height: 350px;
+    height: 300px;
     }
 
     .slide1 {
