8750be635526f3614e6ac9dba3599ed2c7fba920
Caiweiqi <iscaiweiqi@163.com>
2018-02-05 15:40:42 +0800

    jpg

--- fellows17练习/app/src/components/photos/PhotosList.vue
+++ fellows17练习/app/src/components/photos/PhotosList.vue
@@ -1,18 +1,42 @@
 <template>
-    <div>
-
-    </div>
+    <ul class="photoData">
+        <li v-for="(obj,index) in photoData" :key="index">
+            <router-link :to="'/photoDetail/'+index">
+                <img :src="obj.src" alt="">
+            </router-link>
+        </li>
+    </ul>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
+        data() {
+            return {
+                photoData:[]
+            }
+        },
          mounted:function(){
             this.$store.commit('change','rgb(64,224,208)'),
-            this.$store.commit('changes','Photo')
+            this.$store.commit('changes','Photo'),
+
+            axios.get('static/data/photodata.json').then((res)=>{
+               this.photoData = res.data.photoData;
+               this.$store.state.photoData = this.photoData
+            })
         }
     }
 </script>
 
 <style scoped>
-
+    .photoData{
+        overflow: hidden;
+    }
+    .photoData li{
+        float: left;
+        width: 50%;
+    }
+    .photoData img{
+        width: 100%
+    }
 </style>
