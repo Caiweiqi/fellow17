b3af7fce717cf2ba05f25d07053d486bc6150c17
Caiweiqi <iscaiweiqi@163.com>
2018-02-07 22:22:14 +0800

    photo

---
+++ fellows17练习/app/src/components/photos/PhotoDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import VueTouch from "vue-touch";
+    import Vue from 'vue';
+    Vue.use(VueTouch,{name:'v-touch'});
+
+    export default {
+        data() {
+            return {
+                idx: this.$route.params.idx
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
