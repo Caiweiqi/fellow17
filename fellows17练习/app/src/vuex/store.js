975309efb76a5b02fcd2c1c4e67c3006fd4da7c3
Caiweiqi <iscaiweiqi@163.com>
2018-02-09 23:16:54 +0800

    New changelist

---
+++ fellows17练习/app/src/vuex/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+    bgColor:'red',
+    topTitle:''
+}
+
+const mutations = {
+    change(state,bgColor){
+      state.bgColor=bgColor
+     },
+    changes(state,topTitle){
+      state.topTitle=topTitle
+    }
+    
+}
+
+export default new Vuex.Store({
+    state,
+    mutations
+})
